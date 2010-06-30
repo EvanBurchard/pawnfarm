5d289d80c3ad7f66d6a1c46ba622c0eeca20d6e4
evanburchard <evan.burchard@gmail.com>
2010-06-29 23:45:51 -0400

    test for pawn belonging to user

--- spec/models/pawn_spec.rb
+++ spec/models/pawn_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe Pawn do
+  it { should belong_to(:user) }
   it { should have_db_column(:user_id).of_type(:integer).with_options(:null => false) }
   it { should have_db_column(:name).of_type(:string).with_options(:null => false) }
   it { should have_db_column(:twitter_username).of_type(:string).with_options(:null => false) }
