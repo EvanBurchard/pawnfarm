eab0ed1d5bfac3e48c98335c216a99b8758781ed
evanburchard <evan.burchard@gmail.com>
2010-07-13 20:47:56 -0400

    testing

--- app/models/execution.rb
+++ app/models/execution.rb
@@ -43,7 +43,6 @@ class Execution < ActiveRecord::Base
   end
   
   def build_form 
-    puts "got here"
     @turk_form = TurkForm.new(:execution => self, :body => form_body_text)
     @turk_form.save
     seek_candidates
