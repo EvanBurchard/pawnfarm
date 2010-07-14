47f5be9b337337975333b1f4828a5b76e02d29e7
evanburchard <evan.burchard@gmail.com>
2010-07-14 02:44:07 -0400

    testing

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -21,14 +21,14 @@ class Pawn < ActiveRecord::Base
     executions.each { |e| e.execute! }
   end
   
-  def create_executions!
-    schemes.each do |s|
-      @executions = s.executions
-      unless (@executions.map {|e| e.pawn }).include?(self) 
-        Execution.create(:scheme => s, :pawn => self)
-      end
-    end
-  end
+  # def create_executions!
+  #   schemes.each do |s|
+  #     @executions = s.executions
+  #     unless (@executions.map {|e| e.pawn }).include?(self) 
+  #       Execution.create(:scheme => s, :pawn => self)
+  #     end
+  #   end
+  # end
   
   def tweet
     #twitter_account.tweet
