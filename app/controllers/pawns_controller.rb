f50d4c35a80bb7f246a1c6a6d614101a9ff482c2
Sam Gilbert <sam.o.gilbert@gmail.com>
2010-09-18 19:33:15 -0700

    puts in pawns controller

--- app/controllers/pawns_controller.rb
+++ app/controllers/pawns_controller.rb
@@ -60,7 +60,7 @@ class PawnsController < ApplicationController
   end
 
   def authorize_url
-    oauth.set_callback_url("http://sharp-journey-33.heroku.com/#{finalize_pawn_path}")      
+    oauth.set_callback_url("http://sharp-journey-33.heroku.com#{finalize_pawn_path}")      
     session['rtoken'] = @pawn.twitter_account.request_token = oauth.request_token.token
     session['rsecret'] = @pawn.twitter_account.request_secret = oauth.request_token.secret
     @pawn.twitter_account.save
